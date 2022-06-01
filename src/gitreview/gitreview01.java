10bd9179289c974ff0f030288f38032391fa2bae
danoka5 <dana.zhuss@gmail.com>
2022-05-31 21:52:27 -0400

    2nd commit

--- src/gitreview/gitreview01.java
+++ src/gitreview/gitreview01.java
@@ -4,7 +4,10 @@ public class gitreview01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("today we learn github");
+		
+		System.out.print("today is a good day");
+		System.out.print("today is a good day");
 	}
 
 }
